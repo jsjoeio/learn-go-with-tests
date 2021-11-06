31023ea0edae8130c618eb26ed636c4006084ce1
Joe <jjprevite@gmail.com>
2021-11-06 23:18:39 +0000

    progress through hello world

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Joe")
-	want := "Hello, Joe"
-	
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Joe")
+		want := "Hello, Joe"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, world' when an empty string is applied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
