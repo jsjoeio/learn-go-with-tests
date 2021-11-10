383b838a2ceeae9973009a26c688990152699daa
Joe <joe@coder.com>
2021-11-10 23:44:53 +0000

    feat: add support for spanish prefix

--- hello/hello.go
+++ hello/hello.go
@@ -1,16 +1,16 @@
 package main
 
-import "fmt"
-
+const spanish = "Spanish"
+const spanishHelloPrefix = "Hola, "
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
-}
 
-func main() {
-	fmt.Println(Hello("world"))
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+	return englishHelloPrefix + name
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -12,14 +12,20 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Joe")
+		got := Hello("Joe", "")
 		want := "Hello, Joe"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, world' when an empty string is applied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
