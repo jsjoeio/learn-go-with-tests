96d1520c6725b845fd1d341835e07d6faf8adb40
Joe <joe@coder.com>
2021-11-10 23:50:07 +0000

    add italian support

--- hello/hello.go
+++ hello/hello.go
@@ -2,23 +2,27 @@ package main
 
 const spanish = "Spanish"
 const french = "French"
+const italian = "Italian"
 const spanishHelloPrefix = "Hola, "
 const frenchHelloPrefix = "Bonjour, "
 const englishHelloPrefix = "Hello, "
+const italianHelloPrefix = "Ciao, "
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	
+
 	prefix := englishHelloPrefix
-	
+
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
+	case italian:
+		prefix = italianHelloPrefix
 	}
-	
+
 	return prefix + name
 }
