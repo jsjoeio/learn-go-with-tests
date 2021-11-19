5f29bf82cf6689487b70ed877f364b97e7b3c2f7
Joe <joe@coder.com>
2021-11-19 22:30:39 +0000

    feat: add SumAll to arrays

--- arrays/sum.go
+++ arrays/sum.go
@@ -11,3 +11,13 @@ func Sum(numbers []int) int {
 
 // Arrays are fixed in length
 // Slices are not
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
